898baa11413de9a3fd7355a79fe78b9382bb2a4f
CSaguinsin <saguinsincarl8@gmail.com>
2025-02-12 17:15:23 +0800

    Final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "HelmetPro - Next.js App",
+  title: "HelmetPro - Helmet Vendo Cleaning Machine",
   description: "Next.js application for HelmetPro",
 };
 
